8b8cd22db2b308b3030b1a07258924bcf0992f23
Jan Kjetil Myklebust <jan.kjetil.myklebust@cenium.com>
2022-11-15 07:05:24 +0100

    Moves the printing of the menu to its own method.
    Adds xmldoc annotation to the interface's methods.

--- SodaMachine/Services/SodaMachineInterface.cs
+++ SodaMachine/Services/SodaMachineInterface.cs
@@ -6,7 +6,7 @@ namespace SodaMachine.Services
     internal static class SodaMachineInterface
     {
         /// <summary>
-        /// This is the starter method for the service.
+        /// This is the run method for the service.
         /// </summary>
         internal static async Task Run(SodaMachineState machine)
         {
@@ -15,18 +15,7 @@ namespace SodaMachine.Services
             Console.Clear();
             do
             {
-                var inventory = machine.ListInventory();
-
-                Console.WriteLine("\n");
-                Console.WriteLine("Available commands:");
-                Console.WriteLine("insert (money) - Money put into money slot");
-                Console.WriteLine("order ({0}) - Order from machines buttons", inventory);
-                Console.WriteLine("sms order ({0}) - Order sent by sms", inventory);
-                Console.WriteLine("recall - gives money back");
-                Console.WriteLine("quit - turn off the soda machine");
-                Console.WriteLine("-------");
-                Console.WriteLine("Inserted money: {0}", machine.Credit);
-                Console.WriteLine("-------\n\n");
+                PrintMenu(machine);
 
                 var input = Console.ReadLine();
 
@@ -34,6 +23,32 @@ namespace SodaMachine.Services
             } while(run);
         }
 
+        /// <summary>
+        /// Prints the main menu of the machine.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        private static void PrintMenu(SodaMachineState machine)
+        {
+            var inventory = machine.ListInventory();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("insert (money) - Money put into money slot");
+            Console.WriteLine("order ({0}) - Order from machines buttons", inventory);
+            Console.WriteLine("sms order ({0}) - Order sent by sms", inventory);
+            Console.WriteLine("recall - gives money back");
+            Console.WriteLine("quit - turn off the soda machine");
+            Console.WriteLine("-------");
+            Console.WriteLine("Inserted money: {0}", machine.Credit);
+            Console.WriteLine("-------\n\n");
+        }
+
+        /// <summary>
+        /// Handles input from the console.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        /// <param name="input">The input from <see cref="Console.ReadLine"/></param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) HandleInput(SodaMachineState machine, string? input)
         {
             if(string.IsNullOrEmpty(input))
@@ -57,6 +72,12 @@ namespace SodaMachine.Services
             return (true, machine);
         }
 
+        /// <summary>
+        /// Handles the insert credit option.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        /// <param name="input">The input from <see cref="Console.ReadLine"/></param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) HandleInsert(SodaMachineState machine, string input)
         {
             // Add to credit.
@@ -69,6 +90,12 @@ namespace SodaMachine.Services
             return (true, machine);
         }
 
+        /// <summary>
+        /// Handles the ordering of a soda.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        /// <param name="input">The input from <see cref="Console.ReadLine"/></param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) HandleOrder(SodaMachineState machine, string input)
         {
             // Split string on space to get soda name.
@@ -102,6 +129,12 @@ namespace SodaMachine.Services
             return (true, machine);
         }
 
+        /// <summary>
+        /// Handles the ordering of soda by SMS.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        /// <param name="input">The input from <see cref="Console.ReadLine"/></param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) HandleSmsOrder(SodaMachineState machine, string input)
         {
             var sodaName = input.Split(' ')[2];
@@ -125,6 +158,12 @@ namespace SodaMachine.Services
             return (true, machine);
         }
 
+        /// <summary>
+        /// Handles recall of inserted credit.
+        /// </summary>
+        /// <param name="machine">The machine state.</param>
+        /// <param name="input">The input from <see cref="Console.ReadLine"/></param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) HandleRecall(SodaMachineState machine, string input)
         {
             // Give money back.
@@ -134,6 +173,16 @@ namespace SodaMachine.Services
             return (true, machine);
         }
 
+        /// <summary>
+        /// Terminates the application.
+        /// </summary>
+        /// <remarks>
+        /// We're doing this relatively brutally.
+        /// This could be done more elegantly both by using a different dependency
+        /// injection framework and by handling the input in a more refined manner.
+        /// </remarks>
+        /// <param name="machine">The machine state.</param>
+        /// <returns>Run flag and updated machine state</returns>
         private static (bool, SodaMachineState) Quit(SodaMachineState machine)
         {
             Environment.Exit(0);
